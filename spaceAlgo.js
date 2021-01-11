89d178a212838760254dafef1a2c4a8e7b1983a6
Francis Albores <mrthemetribe@gmail.com>
2021-01-11 13:26:05 +0800

    Init

---
+++ spaceAlgo.js
@@ -0,0 +1,28 @@
+const justify = (line, limit) => {
+    const words = line.split(' ');
+    const wordCharCount = words.join('').length;
+    const spaceCount = words.length - 1;
+    const extraSpace = limit - (spaceCount + wordCharCount)
+    const newSpaceCount = Math.floor((extraSpace + spaceCount) / spaceCount)
+
+    let ret = "";
+    let currentCharCounter = 0;
+    if(wordCharCount < limit) { 
+
+        
+        
+        words.forEach((word, index) => {
+            ret += word;
+            if((ret.length + newSpaceCount) < limit) {
+                for(let x = 0; x < newSpaceCount; x++) {
+                    ret += " ";
+                }
+            }
+        })
+    }
+    document.getElementById('result').innerHTML = 
+        "Line : " + line + '\n' +
+        "Limit : " + limit + '\n' +
+        "Result : " + ret;
+} 
+
